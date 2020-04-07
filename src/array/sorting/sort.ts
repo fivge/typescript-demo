2c07835605fe718fd57a55876158b5a529fdd04a
fivge <luanxingtong@gmail.com>
2020-04-07 14:37:47 +0800

    fix(array): rm sorting temp

--- src/array/sorting/sort.ts
+++ src/array/sorting/sort.ts
@@ -1,24 +1,24 @@
-export class Sort {
-  constructor(originalCallbacks) {
-    this.callbacks = Sort.initSortingCallbacks(originalCallbacks);
-    this.comparator = new Comparator(this.callbacks.compareCallback);
-  }
-  Comparator;
-  /**
-   * @param {SorterCallbacks} originalCallbacks
-   * @returns {SorterCallbacks}
-   */
-  static initSortingCallbacks(originalCallbacks) {
-    const callbacks = originalCallbacks || {};
-    const stubCallback = () => {};
+// export class Sort {
+//   constructor(originalCallbacks) {
+//     this.callbacks = Sort.initSortingCallbacks(originalCallbacks);
+//     this.comparator = new Comparator(this.callbacks.compareCallback);
+//   }
+//   Comparator;
+//   /**
+//    * @param {SorterCallbacks} originalCallbacks
+//    * @returns {SorterCallbacks}
+//    */
+//   static initSortingCallbacks(originalCallbacks) {
+//     const callbacks = originalCallbacks || {};
+//     const stubCallback = () => {};
 
-    callbacks.compareCallback = callbacks.compareCallback || undefined;
-    callbacks.visitingCallback = callbacks.visitingCallback || stubCallback;
+//     callbacks.compareCallback = callbacks.compareCallback || undefined;
+//     callbacks.visitingCallback = callbacks.visitingCallback || stubCallback;
 
-    return callbacks;
-  }
+//     return callbacks;
+//   }
 
-  sort() {
-    throw new Error("sort method must be implemented");
-  }
-}
+//   sort() {
+//     throw new Error("sort method must be implemented");
+//   }
+// }
